53fc9ffcbbcdefb819112b04247f99f767a36ef3
lilyclemson <lilix@g.clemson.edu>
2020-04-29 09:07:51 -0400

    working testCopy

--- NodeJS/testCopy.js
+++ NodeJS/testCopy.js
@@ -1,7 +1,6 @@
 const gitP = require('simple-git/promise');
 const REPO = 'https://github.com/CSSEGISandData/COVID-19.git';
-var path = require('path');
-
+const path = require('path');
 // gitP().silent(true)
 //   .clone(REPO)
 //   .then(() => console.log('finished'))
@@ -12,11 +11,16 @@ const fs = require('fs');
 let request = require('request-promise');
 
 
+// git('./COVID-19')
+// git
+//   .fetch('upstream', 'master', console.log.bind(console))
+//   .diff(['master', 'upstream/master', '--name-only'] ,  console.log.bind(console));
+
+
 let fileList = [];
-let todaysFileName = '' ;
 git
   .exec(() => console.log('Starting pull...'))
-  .pull('origin', 'master',(err, update) => {
+  .pull('origin', 'master' ,(err, update) => {
       console.log(update);
       if(update && update.summary.changes) {
         fileList = update.files;
@@ -24,50 +28,60 @@ git
       }
    })
   .exec(() => {
-    fileList.forEach((item) => {
-      console.log(item);
-      todaysFileName = item
-      upload();
+    console.log('pull done.');
+    fileList.forEach( async (item) => {
+      console.log(typeof(item));
+      if(item.startsWith('csse_covid_19_data/csse_covid_19_daily_reports/') === true
+         && item.search('.csv') != -1){
+        let uploadResponse = await upload(item);
+      }
     });
   });
 
 
-let upload = () => {
+let upload = (filename) => {
+  let today = new Date();
+  let todaysFileName = filename;
+  // let todaysFileName = ("0" + (today.getMonth() + 1)).slice(-2) + '-' + ("0" + today.getDate()).slice(-2) + '-' + today.getFullYear()+'.csv';
+  // let todaysFilePath = path.join(__dirname, 'COVID-19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'+todaysFileName);
   let todaysFilePath = path.join(__dirname, 'COVID-19/' + todaysFileName);
-  if (fs.existsSync(todaysFilePath)) {
-    let fileLocation;
-    let _destPath = '.';
-    let _clusterAddrAndPort = 'http://10.173.147.1:8010';
-    let _ClusterIP = '10.173.147.1';
-    let _filename = todaysFileName,
-      _mimetype = 'text/csv',
-      _fileStream = fs.createReadStream(todaysFilePath),
-      _clusterFilename = path.basename(todaysFileName);
-
-      request({
-        method: 'POST',
-        uri: _clusterAddrAndPort + '/Filespray/UploadFile.json?upload_' +
-          '&NetAddress=' + _ClusterIP + '&rawxml_=1&OS=2&' +
-          // 'Path=/var/lib/HPCCSystems/mydropzone/hpccsystems/covid19/file/raw/johnhopkins/v2/',
-          'Path=/var/lib/HPCCSystems/mydropzone/Incoming-FWLogs/Incoming/',
-        formData: {
-          'UploadedFiles[]': {
-            value: _fileStream,
-            options: {
-              filename: _clusterFilename,
-              contentType: _mimetype
-            }
+  return new Promise((resolve, reject) => {
+    if (fs.existsSync(todaysFilePath)) {
+      let _destPath = '.';
+      let _clusterAddrAndPort = 'http://40.71.7.106:8010';
+      let _ClusterIP = '10.0.0.6';
+      let _filename = todaysFileName,
+        _mimetype = 'text/csv',
+        _fileStream = fs.createReadStream(todaysFilePath),
+        _clusterFilename = path.basename(todaysFileName);
+7
+        request({
+          method: 'POST',
+          uri: _clusterAddrAndPort + '/Filespray/UploadFile.json?upload_' +
+            '&NetAddress=' + _ClusterIP + '&rawxml_=1&OS=2&' +
+            'Path=/var/lib/HPCCSystems/mydropzone/hpccsystems/covid19/file/raw/johnhopkins/v2/',
+          formData: {
+            'UploadedFiles[]': {
+              value: _fileStream,
+              options: {
+                filename: _clusterFilename,
+                contentType: _mimetype
+              }
+            },
           },
-        },
-        resolveWithFullResponse: true
-      }).then((response) => {
-        console.log(response.body)
-        console.log('End of upload')
-      }).catch((err) => {
-        console.log(err);
-      })
+          resolveWithFullResponse: true
+        }).then((response) => {
+          console.log(response.body);
+          console.log('End of upload');
+          resolve(response);
+        }).catch((err) => {
+          console.log(err);
+          reject(err);
+
+        })
+      }
     }
-}
+)}
 
 
 

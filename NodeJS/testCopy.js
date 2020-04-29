0bedf2e490957f8591ed060638c88fca635bd4a1
lilyclemson <lilix@g.clemson.edu>
2020-04-29 15:56:28 -0400

    working upload with auth

--- NodeJS/testCopy.js
+++ NodeJS/testCopy.js
@@ -1,6 +1,7 @@
 const gitP = require('simple-git/promise');
 const REPO = 'https://github.com/CSSEGISandData/COVID-19.git';
 const path = require('path');
+
 // gitP().silent(true)
 //   .clone(REPO)
 //   .then(() => console.log('finished'))
@@ -9,13 +10,6 @@ const path = require('path');
 const git = require('simple-git')('./COVID-19');
 const fs = require('fs');
 let request = require('request-promise');
-
-
-// git('./COVID-19')
-// git
-//   .fetch('upstream', 'master', console.log.bind(console))
-//   .diff(['master', 'upstream/master', '--name-only'] ,  console.log.bind(console));
-
 
 let fileList = [];
 git
@@ -40,26 +34,23 @@ git
 
 
 let upload = (filename) => {
-  let today = new Date();
   let todaysFileName = filename;
-  // let todaysFileName = ("0" + (today.getMonth() + 1)).slice(-2) + '-' + ("0" + today.getDate()).slice(-2) + '-' + today.getFullYear()+'.csv';
-  // let todaysFilePath = path.join(__dirname, 'COVID-19/COVID-19/csse_covid_19_data/csse_covid_19_daily_reports/'+todaysFileName);
   let todaysFilePath = path.join(__dirname, 'COVID-19/' + todaysFileName);
   return new Promise((resolve, reject) => {
     if (fs.existsSync(todaysFilePath)) {
       let _destPath = '.';
-      let _clusterAddrAndPort = 'http://40.71.7.106:8010';
+      let _clusterAddrAndPort = 'http://40.71.7.106:8010/';
       let _ClusterIP = '10.0.0.6';
       let _filename = todaysFileName,
         _mimetype = 'text/csv',
         _fileStream = fs.createReadStream(todaysFilePath),
         _clusterFilename = path.basename(todaysFileName);
-7
         request({
           method: 'POST',
+          auth: {'user':'xulili01', 'password':'Q4dRtHRF'},
           uri: _clusterAddrAndPort + '/Filespray/UploadFile.json?upload_' +
             '&NetAddress=' + _ClusterIP + '&rawxml_=1&OS=2&' +
-            'Path=/var/lib/HPCCSystems/mydropzone/hpccsystems/covid19/file/raw/johnhopkins/v2/',
+            'Path=/var/lib/HPCCSystems/mydropzone/hpccsystems/covid19/file/raw/JohnHopkins/V2/',
           formData: {
             'UploadedFiles[]': {
               value: _fileStream,
